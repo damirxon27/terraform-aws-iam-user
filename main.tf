cadf9215704b530b146db422acb58ba481f24f0b
damirxon27 <damirxon27@gmail.com>
2024-08-07 18:24:40 -0400

    first version

---
+++ main.tf
@@ -0,0 +1,14 @@
+resource "aws_iam_user" "users" {
+  for_each = var.users
+  name     = each.key
+  path     = "/"
+}
+variable "users" {
+  
+  type        = map(string)
+}
+
+output "user_name" {
+    value = [for user in aws_iam_user.users : user.name]
+  
+}
